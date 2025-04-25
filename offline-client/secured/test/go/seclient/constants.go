e7609076c834d4791447dd1081063f79e9a98124
ceyewan <1020470197@qq.com>
2025-04-25 12:42:58 +0800

    调整了 secured 部分,地址长度从 64 调整到 20

--- offline-client/secured/test/go/seclient/constants.go
+++ offline-client/secured/test/go/seclient/constants.go
@@ -16,7 +16,7 @@ const (
 
 	// 固定长度常量
 	USERNAME_LENGTH      = 32 // 用户名长度
-	ADDR_LENGTH          = 64 // 地址长度
+	ADDR_LENGTH          = 20 // 地址长度
 	MESSAGE_LENGTH       = 32 // 消息长度
 	MAX_SIGNATURE_LENGTH = 72 // DER格式签名最大长度
 	MIN_SIGNATURE_LENGTH = 70 // DER格式签名最小长度
