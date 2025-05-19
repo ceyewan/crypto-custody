4d752afe430fe84d1d11f05b98358b7404c2a27d
ceyewan <ceyewan@gmail.com>
2025-05-19 11:16:47 +0800

    feat: 删除冗余的账户服务文件，功能已迁移至eth_service.go

--- online-server/service/account.go
+++
@@ -1,5 +0,0 @@
-package service
-
-// This file is intentionally left empty
-// All account-related functionality has been moved to eth_service.go
-// The file exists to satisfy imports that might be referring to it
